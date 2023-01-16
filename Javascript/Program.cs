a38c4ea9898af2df5eb0e04050facab738f821c5
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2023-01-16 21:50:16 +0300

    Add project files.

---
+++ Javascript/Program.cs
@@ -0,0 +1,19 @@
+namespace Javascript
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+
+            app.UseStaticFiles();
+
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
---
+++ Javascript/wwwroot/harici.js
@@ -0,0 +1,45 @@
+﻿
+function Hesapla() {
+
+    /*debugger;*/
+
+    var sayi1 = Number(document.getElementById("txtSayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("txtSayi2").value);
+    // isNotaNumber
+    if (isNaN(sayi1) || isNaN(sayi2))
+        return;
+
+    var result = sayi1 + sayi2;
+    document.getElementById("Result").innerHTML = result;
+}
+
+var i = 1;
+function Sayac() {
+    document.getElementById("sayac").innerHTML = i;
+    i++;
+}
+
+// timer görevi görür....
+setInterval(function () {
+    Sayac();
+}, 1000);
+
+
+var syc = 0;
+var tmr = setInterval(function () {
+
+
+    var btn = document.createElement("input"); // button
+    btn.type = "button";
+    btn.value = "btn " + syc;
+
+    document.getElementById("timer").appendChild(btn);
+
+    if (syc == 10) {
+        clearInterval(tmr);
+    }
+
+    syc++;
+
+}, 1000);
