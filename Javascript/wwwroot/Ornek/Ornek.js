2290a5efaf9272a9d674d9fb5a9b5fa74e18b223
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2023-01-17 21:36:33 +0300

    jquery örnek

---
+++ Javascript/wwwroot/Ornek/Ornek.js
@@ -0,0 +1,21 @@
+﻿
+
+
+var i = 1;
+function Slider() {
+    /*var dv = $("#dvS");*/
+
+
+
+    var li = $(".list-group-item");
+    /* console.log(li);*/
+    if (i >= li.length)
+        i = 0;
+
+    var r = i * 41;
+    /*$("#dvS").css("top", "-" + r + "px");*/
+    $("#dvS").animate({
+        "top": "-" + r + "px"
+    }, 2000);
+    i++;
+}
